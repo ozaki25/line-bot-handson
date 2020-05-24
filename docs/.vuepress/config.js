01e5b628b070243bbc88ca086ca5f660498fb37c
ozaki <fs65ugumori@yahoo.co.jp>
2020-05-25 01:47:49 +0900

    update agenda

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'LINE Bot Handson',
   themeConfig: {
     domain: 'https://line-bot-handson.ozaki25.now.sh',
-    sidebar: ['/page1', 'page2', 'page3', 'page4', 'page5'],
+    sidebar: ['/page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
   },
   markdown: {
     lineNumbers: true,
